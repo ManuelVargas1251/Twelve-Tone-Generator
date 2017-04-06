d325f3e55cb2f19e9becc99c5181e21dc0c30fff
manuelvargas1251 <manuelvargas1251@gmail.com>
2017-04-06 14:55:59 -0500

    added toggle

--- gen.js
+++ gen.js
@@ -1,31 +1,40 @@
 //globals
-var _sharps = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
-var _flats = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'A'];
-var _x = 3
-var _y = 4
+var _sharps = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+var _flats = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'A']
+var _alphabet = []	//empty array; will fill w/ sharps or flats 
 var _accidental = "sharps"	//default is sharps
+var _flats_on = false
 
-function mypop(note){
-	var index = _sharps.indexOf(note);
-	
+//remove selected note from alphabet array
+function remove_note(note){
+	var index = _alphabet.indexOf(note);
 	if (index > -1) 
-		_sharps.splice(index, 1);
+		_alphabet.splice(index, 1);
 	console.log(note + "%c has been popped", "color: #009A00")
 }
 
-function choose_accidental(){
-	console.log("Want Sharps or Flats?")
-	//ask what they want
-	console.log("You've selected sharps!")
-}
 function get_element(num_left){
 	str1 = "note_"
 	num1 = 12-num_left
 	return element_id =  str1.concat(num1.toString())
 }
 
-function find_repeat(note){
-	
+function toggle_notes(cb){
+	if(cb.checked){
+		_flats_on=true
+		console.log("You've selected flats!")
+	}
+	else{
+		_flats_on=false
+		console.log("You've selected sharps!")
+	}
+}
+
+function choose_accidental(){
+	if(_flats_on)
+		_alphabet = _flats.concat();
+	else
+	_alphabet = _sharps.concat();
 }
 
 function gen(){
@@ -33,29 +42,31 @@ function gen(){
 	var element_id
 	choose_accidental()
 	
-	while(num_left){
+	while(num_left){	//loop 12 times
 		var random_note = Math.floor((Math.random()* 12))
-		var note = _sharps[random_note]
+		var note = _alphabet[random_note]
+		
 		while(typeof note == 'undefined'){
 			random_note = Math.floor((Math.random()* 12))
-			note = _sharps[random_note]
+			note = _alphabet[random_note]
+			console.log("loop")		//max 39 loops in my tests
+			//this is not the optimal way to find the number
+			//better way would be to also remove the number
+			//that had been called before so random won't call it again
 		}
-
-		element_id = get_element(num_left)
 		
-		//console.log("%celement: "+element_id, "color: #551188")
+		element_id = get_element(num_left)
 		document.getElementById(element_id).innerHTML = note;
+		//console.log("%celement: "+element_id, "color: #551188")
 		//console.log("%cnote: "+note, "color: #2244f2")	
-		mypop(note)
-		num_left -= 1
+		remove_note(note)
+		console.log("alpha: " + _alphabet)
+		num_left -= 1	//decrement
 	}
-	
-	//reinitialize
-	_sharps = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
-	_flats = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'A'];
+	_alphabet = []	//unnecessary
 }
 
-//button to show 
+//create a row
 function create(){
 	gen()
 }
--- readme.md
+++ readme.md
@@ -1,9 +1,17 @@
 ![Programming language](https://img.shields.io/badge/Language-Javascript-black.svg)
 ![Build Status](https://img.shields.io/badge/Build-Passing-brightgreen.svg)
-![Version](https://img.shields.io/badge/Version-0.1-blue.svg?style=flat)
+![Version](https://img.shields.io/badge/Version-1.1-blue.svg?style=flat)
 
 # Twelve Tone Generator
 This program displays a random twelve tone row for twelve tone music. I plan to make a matrix later on and add sound with p5.
 
 ## Demo
 [Click Here](http://manuelvargas.me/Twelve-Tone-Generator/)
+
+## Change Log
+
+1.1 - 4/6/17
+* added flats or sharps toggle
+
+1.0 - 4/6/17
+* initial commit
