4c4d8c66192bd28668b53f95a1091c9f13f76ec5
Manuel Vargas <manuelvargas@my.unt.edu>
2017-10-10 17:09:52 -0500

    1.6.1
    converting this branch into the next update. look in changelog for
    updates

--- js/tone_row.js
+++ js/tone_row.js
@@ -52,54 +52,54 @@ function load_array(){
 
 //main function for tone_row
 function tone_row(){
-	console.log("%ctone_row()%c\t\tstart", "color: blue; font-weight:bold;", "color: orange");
+	console.log("%ctone_row()%c\t\tstart", "color: blue; font-weight:bold;", "color: orange")
 	
 	//this loads the correct musical alphabet
 	//depening on the settings, alphabet can be sharps or flats
-	load_array();
+	load_array()
 	
 	//makes a local copy of the alphabet to use
 	//after each loop, arrays shrinks by one
-	var shrinking_alphabet = _alphabet.slice();
+	var shrinking_alphabet = _alphabet.slice()
 	
 	//if user input a custom row
 	if(custom){
 		//take user input and store it in the _tone_row
-		console.log("%c::custom row?::", "color: #009A00");
-		custom_row();	//user can input their own tone row instead of random
+		console.log("%c::custom row?::", "color: #009A00")
+		custom_row()	//user can input their own tone row instead of random
 		//test row 
 		if(_flats_on == true){
-			_tone_row = ['B','Bb','G','Db','Eb','C','D','A','Gb','E','Ab','F'];
+			_tone_row = ['B','Bb','G','Db','Eb','C','D','A','Gb','E','Ab','F']
 		}
 		else{
-			_tone_row = ['B','A#','G','C#','D#','C','D','A','F#','E','G#','F'];
+			_tone_row = ['B','A#','G','C#','D#','C','D','A','F#','E','G#','F']
 		}
-		console.log("tone row (inner): " + _tone_row);
+		console.log("tone row (inner): " + _tone_row)
 	}
 	else{
 		for(var i = 12 ; i > 0 ; i--){
 			//random int from 0 to length of array
-			var random_address = Math.floor((Math.random() * i ));
-			var note = shrinking_alphabet[random_address];	//select note based on address
+			var random_address = Math.floor((Math.random() * i ))
+			var note = shrinking_alphabet[random_address]	//select note based on address
 			
 			//create and store element id
 			//send note to html
 			//document.getElementById(get_element_id(i)).innerHTML = note
 			
 			//store note in final array to use later
-			_tone_row.push(note);	
+			_tone_row.push(note)
 			
 			//remove current note from current alphabet array
-			remove_note(note, shrinking_alphabet);
+			remove_note(note, shrinking_alphabet)
 		}
 	}
 	
 	//regardless of what happened, print tone row to #main-panel
 	_tone_row.forEach(function(tone){
-		document.getElementById(get_element_id(_tone_row.indexOf(tone))).innerHTML = tone;
+		document.getElementById(get_element_id(_tone_row.indexOf(tone))).innerHTML = tone
 	})
 	
 	
 	//console.log("tone row: " + _tone_row)
-	console.log("%ctone_row()%c\t\tdone","color: blue; font-weight:bold;", "color: limegreen");
+	console.log("%ctone_row()%c\t\tdone","color: blue; font-weight:bold;", "color: limegreen")
 }
